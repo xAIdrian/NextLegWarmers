8d09d2e105bef3b3a9ab62b6e8d841b13a12120a
Adrian Mohnacs <amohnacs@gmail.com>
2025-09-24 21:49:23 +0700

    compiler and linter updated

---
+++ src/app/marketing/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MarketingLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+	<div>
+	  {children}
+	</div>
+  );
+};
+
+export default MarketingLayout;
